5e42ebeab951c12d210a89ac1f54415fef16a631
Komal-Kalsariya <komalkalsariya233@gmail.com>
2024-06-26 21:48:18 +0530

    react-props

--- src/UserProfileCard.jsx
+++ src/UserProfileCard.jsx
@@ -3,26 +3,32 @@ import { useState } from "react"
 const UserProfileCard=({name,age,bio,location, profileImg})=>{
 
     let [follow,setfollow]=useState(false)
+    // let [biodata,setbiodata]=useState(bio)
+    // const handlebiodata=()=>{
+    //     setbiodata(biodata)
+    // }
+    
     
     const handlefollow=()=>{
         setfollow(!follow)
     };
-    
+
 
 
     return(
         <div>
 
         <h1>Name:{name}</h1>
-        <h3>Age:{age}<br/>Bio:{bio}<br />Location:{location}</h3>
+        <h3>Age:{age}<br />Location:{location}</h3>
         <img src={ profileImg} alt="" className="img1" />
-       
+        <p> Bio:{bio}
+            <button>readmore</button> </p>
+
+
        <button onClick={handlefollow} >{follow ? "Unfollow" : "follow"}</button>
+     
       
-       
-
-
-        </div>
+       </div>
     )
 }
 export default UserProfileCard 
